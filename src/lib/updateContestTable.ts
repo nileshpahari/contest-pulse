1022dd7a1e182a3c64656c3ae9e95aa2deebcfcf
Nilesh Pahari <nileshpahari@protonmail.com>
2025-05-03 03:59:50 +0530

    fix bug in upcomingContest fetching logic (change to upsert)

--- src/lib/updateContestTable.ts
+++ src/lib/updateContestTable.ts
@@ -1,49 +1,66 @@
 import axios from "axios";
 import db from "../db/index.js";
-const fetchUrl = "https://competeapi.vercel.app/contests/upcoming/";
+import { URL } from ".././constants.js";
 
-  function formatDuration(ms: number): string {
-    const hours = Math.floor(ms / 3600000);
-    const minutes = Math.floor((ms % 3600000) / 60000);
-    const seconds = Math.floor((ms % 60000) / 1000);
-    return `${hours}h ${minutes}m ${seconds}s`;
-  }
+function formatDuration(ms: number): string {
+  const hours = Math.floor(ms / 3600000);
+  const minutes = Math.floor((ms % 3600000) / 60000);
+  const seconds = Math.floor((ms % 60000) / 1000);
+  return `${hours}h ${minutes}m ${seconds}s`;
+}
 
-  function formatTime(time: number): Date {
-    return new Date(time);
-  } 
+function formatTime(time: number): Date {
+  return new Date(time);
+}
 
-  const addUpcoming = async () => {
-    const res = (await axios.get(fetchUrl)).data;
-    const upcomingContest = res.map((contest: any) => {
-        return {
-            site: contest.site,
-            title: contest.title,
-            startTime: formatTime(contest.startTime),
-            endTime: formatTime(contest.endTime),
-            duration: formatDuration(contest.duration),
-            url: contest.url,
-            isPast: false
-    }})
-    await db.contest.createMany({
-        data: upcomingContest,
-        skipDuplicates: true
-    })
-};
-
-    const togglePastContest= async () => {
-       await db.contest.updateMany({
-        where: {
-            endTime: {
-                lt: new Date()
-            }
+const addUpcoming = async () => {
+  const res = (await axios.get(URL)).data;
+  const upcomingContest = res.map((contest: any) => {
+    return {
+      site: contest.site,
+      title: contest.title,
+      startTime: formatTime(contest.startTime),
+      endTime: formatTime(contest.endTime),
+      duration: formatDuration(contest.duration),
+      url: contest.url,
+    };
+  });
+//   await db.contest.createMany({
+//     data: upcomingContest,
+//     skipDuplicates: true,
+//   });
+  await Promise.all(
+    upcomingContest.map((contest: any) =>
+      db.contest.upsert({
+        where: { url: contest.url },
+        create: { ...contest, isPast: false },
+        update: {
+          startTime: contest.startTime,
+          endTime: contest.endTime,
+          duration: contest.duration,
+          isPast: contest.endTime < Date.now(),
         },
-        data: {
-            isPast: true
-        }
-       }) 
-    }
-export const updateContestTable = async () => {
-    await togglePastContest();
-    await addUpcoming();
+      })
+    )
+  );
 };
+
+const togglePastContest = async () => {
+  await db.contest.updateMany({
+    where: {
+      endTime: {
+        lt: new Date(),
+      },
+      isPast: false,
+    },
+    data: {
+      isPast: true,
+    },
+  });
+};
+export const updateContestTable = async () => {
+  await togglePastContest();
+  await addUpcoming();
+};
+
+updateContestTable();
