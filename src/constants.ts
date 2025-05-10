ee8587943175d147cd5f727978c98f78adf48b41
Nilesh Pahari <nileshpahari@protonmail.com>
2025-05-11 03:22:38 +0530

    add .env bugfix

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,8 @@
+import "dotenv/config";
 export const URL = process.env.fetchURL as string;
+export const NEXTAUTH_URL= process.env.NEXTAUTH_URL as string;
+export const NEXTAUTH_SECRET= process.env.NEXTAUTH_SECRET as string;
+export const GOOGLE_CLIENT_ID= process.env.GOOGLE_CLIENT_ID as string;
+export const GOOGLE_CLIENT_SECRET= process.env.GOOGLE_CLIENT_SECRET as string;
+export const DB_URL= process.env.DATABASE_URL as string;
+export const YT_KEY =  process.env.NEXT_PUBLIC_YOUTUBE_API_KEY  as string;
