4dbf21d4aca73bd5040df3f9821fe73922f86a56
Nilesh Pahari <nileshpahari@protonmail.com>
2025-06-12 21:32:25 +0530

    add console log to fetch function

--- src/app/api/fetch/route.ts
+++ src/app/api/fetch/route.ts
@@ -3,5 +3,6 @@ import { updateContestTable } from '@/lib/updateContestTable';
 
 export async function GET() {
   const succeeded:boolean = await updateContestTable();
+   console.log("Scheduled job ran at", new Date().toISOString());
   return NextResponse.json({ success: succeeded });
 }
