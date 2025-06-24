4364c9a20888207e7ae9cc6640b0bb7770e88a45
Nilesh Pahari <nileshpahari@protonmail.com>
2025-06-24 23:03:30 +0530

    bug fix and cleaup next-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,60 +1,22 @@
-import CredentialsProvider from "next-auth/providers/credentials";
+import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import client from "@/db";
-import bcrypt from "bcryptjs";
-
-
-interface User {
-  id: string;
-  email: string;
-  firstName: string;
-  lastName: string | null;
-}
-
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import db from "@/db/index";
+import { Adapter } from "next-auth/adapters";
 
 const NEXT_AUTH_CONFIG = {
-      providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", placeholder: "john@john.com", type: "text" },
-        password: {
-          label: "Password",
-          placeholder: "p@SSworD",
-          type: "password",
-        },
-      },
-      async authorize(credentials: any): Promise<User | null> {
-        if (!(credentials?.email && credentials?.password)) return null;
-        const user = await client.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
-        if (!user || !user.password) return null;
-        const isPassValid = await bcrypt.compare(
-          credentials.password,
-          user.password
-        );
-        if (!isPassValid) return null;
-        const { id, firstName, lastName, email } = user;
-        return {
-          id: String(id),
-          email,
-          firstName,
-          lastName,
-        };
-      },
-    }),
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-
-  }
 }
 
 export default NEXT_AUTH_CONFIG
