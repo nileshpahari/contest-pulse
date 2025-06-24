49171a7f857f9b1335ed065bf8f57c9a0f286bf5
Nilesh Pahari <nileshpahari@protonmail.com>
2025-06-25 01:31:48 +0530

    bugfix add-remainder route

--- src/app/api/add-reminder/route.ts
+++ src/app/api/add-reminder/route.ts
@@ -1,32 +1,31 @@
 import db from "@/db";
-import { sendEmail } from "@/lib/send";
 import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(){
-  return NextResponse.json({success:false}, {status: 500})
-  // try {
-  // const {contest} = await req.json();
-  // // await sendEmail(contest.email, contest);
-  // await db.reminder.create({
-  //   data: {
-  //     notifyAt: new Date(contest.startTime),
-  //     notified: false,
-  //     user: {
-  //       connect: {
-  //         email: contest.email
-  //       }
-  //     },
-  //     contest: {
-  //       connect: {
-  //         id: contest.id
-  //       }
-  //     }
-  //   }
-  // }) 
-  // console.log(contest)
-  // return NextResponse.json({success: true}, {status: 200});
-  // } catch (error) {
-  //   console.log(error);
-  //   return NextResponse.json({success: false}, {status: 500});
-  // }
+import { getServerSession } from "next-auth";
+import auth from "@/lib/auth";
+import { Prisma } from "@prisma/client";
+export async function POST(req: NextRequest){
+  try {
+  const session = await getServerSession(auth)
+  if(!session){
+    return NextResponse.json({success: false, message: "Unauthorized"}, {status: 401});
+  }
+  const email = session.user?.email as string
+  const {contest} = await req.json();
+  await db.notification.create({
+    data: {
+      notifyAt: new Date(contest.startTime),
+      notified: false,
+      userEmail: email,
+      contestId: contest.id,
+    }
+  }) 
+  console.log(contest)
+  return NextResponse.json({success: true, message: "Email reminder added"}, {status: 200});
+  } catch (error) {
+    console.log(error)
+    if(error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002"){
+      return NextResponse.json({success: false, message: "Email reminder for this contest already exists"}, {status: 409});
+    }
+    return NextResponse.json({success: false, message: "Failed to add email reminder"}, {status: 500});
+  }
 }
